5ff7c55831c61ccc4fe91ddc1e2f9e34d3c01e83
alejo281 <alejo.281@gmail.com>
2018-07-27 23:44:48 +0000

    Changed everything to a Class, and added a few methods

--- watchdog.py
+++ watchdog.py
@@ -1,53 +1,111 @@
 #!/usr/bin/python
+import logging
 import sys
-import serial
 import time
+import serial
+import argparse
+import threading
+from datetime import timedelta
 
 
-def run():
-  '''
-  Interval ( in seconds ) = n/10
-  This number will always be rounded to the closest integer.
-  '''
-  interval = 1 
-  
-  try:
-    user_interval = int(sys.argv[2])/10
-    if 0 < user_interval < 360:
-      interval = user_interval
-  except Exception as e:
-    # Interval seems invalid. Let's ignore it.
-    pass
-  print "Heartbeat configured for {} second(s) intervals".format((interval*10))
-  while True:
-    watchdog.write(chr(interval))
-    watchdog.flush()
-    time.sleep(1)
+class UsbWatchDog(object):
 
-usage = '''{} <port> [ <heartbeat_interval> ]
+    def __init__(self, port, heartbeat=10, baud=9600):
+        self.heartbeat = heartbeat if 10 < heartbeat <= 360 else 10
+        self.port = port
+        self.watchdog = serial.Serial(self.port, baud)
+        self.run
+        
+    def get_info(self):
+        ''' TODO: get current system info
+        '''
+        with open('/proc/uptime', 'r') as f:
+            uptime = float(f.readline().split()[0])
+            last_boot = str(timedelta(seconds = uptime))
 
-                  port: Serial port to use ( e.g. /dev/cu.wchusbserial1420 )
-    heartbeat_interval: maximum amount of time without a hearbeat ( e.g. 180 seconds. 10 second increments only )
-'''.format(sys.argv[0])
+        scheduled_restart = 0
+        info = {
+            'last_boot':last_boot,
+            'scheduled_restart': 0,
+            'timeout':self.heartbeat
+        }
+        return info
+        
+    def run(self):
+        ''' Interval ( in seconds ) = n/10,
+        This number will always be rounded to the closest integer. '''
+        try:
+            interval = int(self.heartbeat)/10
+        except Exception as e:
+            logging.warn("Interval seems invalids. Error {}".format(e))
 
-def reset():
-  watchdog.write(chr(255))
-  watchdog.flush()
-  time.sleep(1)
+        logging.debug ("Heartbeat configured for {} second(s) intervals"
+                       .format(interval*10))
+        while True:
+            self.watchdog.write(chr(interval))
+            self.watchdog.flush()
+            time.sleep(1)
 
-def checkInternet():
-  import urllib2
-  try:
-    urllib2.urlopen('http://google.com', timeout=5)
-    return True
-  except urllib2.URLError as err: 
-    return False
+    def reset_now(self):
+        ''' Restart Now
+        '''
+        logging.debug('Restart Now')
+        self.watchdog.write(chr(255))
+        self.watchdog.flush()
+        time.sleep(1)
 
-if len(sys.argv)> 1:
-  watchdog = serial.Serial(sys.argv[1], 9600)
-  time.sleep(1)
-  run()
-else:
-  print usage
+    def change_timeout_seconds(self, timeout):
+        ''' Change Heartbeat timeout
+        '''
+        logging.debug('Changing heart beat from {} to {}'
+                      .format(self.heartbeat, timeout))
+        try:
+            self.heartbeat = int(timeout) if 10 < int(timeout) <= 360 else 10
+        except ValueError as e:
+            logging.warning('Invalid type, integer is required. Error {}'.format(e))
+            raise TypeError
 
+    def scheduled_restart(self):
+        ''' TODO: scheduled restart method
+        '''
+        pass
+
+    def check_internet(self):
+        ''' Test internet connection
+        '''
+        import urllib2
+        try:
+            urllib2.urlopen('http://google.com', timeout=5)
+            return True
+        except urllib2.URLError as e: 
+            return False
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
+    parser = argparse.ArgumentParser(
+        description='Python Script to allow you to control a usb watchdog')
+    parser.add_argument('port', type=str, 
+                        help='Serial port to use (e.g /dev/cu.wchussetila420)')
+    parser.add_argument('--hb', nargs='?', const=10, type=int, 
+                        help='Maximum amount of time without a hearbeat '
+                        '(e.g. 180 seconds). 10 second increments only. '
+                        'Default: 10 seconds, Max: 360')
+    args = parser.parse_args()
+
+    heartbeat = args.hb
+    port = args.port
+
+    try:
+        device = UsbWatchDog(port, heartbeat)
+        logging.debug('Device Information {}'.format(device.get_info()))
+        time.sleep(2)
+        device.change_timeout_seconds(140)
+        time.sleep(1)
+        logging.debug('Device Information {}'.format(device.get_info()))
+        while True:
+            pass
+    except (KeyboardInterrupt, SystemExit):
+        logging.warning("Keyboard interrupt")
+        sys.exit()
 
